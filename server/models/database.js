c7529434a8167f8354c7881f710eaff6f84d3cf2
InbalBu <inbalbu2@gmail.com>
2022-07-28 12:53:44 +0300

    deleted

--- server/models/database.js
+++ server/models/database.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
-
-try {
-    mongoose.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    console.log('MongoDB is Connected...');
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
